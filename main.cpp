5fb97b42c0177048d6519c11e6861cef0668786a
rene.rentsch <rene.rentsch@hotmail.com>
2021-10-08 21:25:37 +0200

    blabla

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <cstdlib>
 #include <iostream>
+#include "module.h"
 
 using namespace std;
 
 int main() {
-	cout << "Essai 2023" << endl;
+	cout << "Essai 2024" << endl;
+	cout << incremente(3, 5) << endl;
+	cout << "Hello" << endl;
 	return EXIT_SUCCESS;
 }
